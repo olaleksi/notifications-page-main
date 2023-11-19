e5c0a7af66504f08d7a79cb3224d7f3aae0dd6e1
Olalekan olajide <72236651+olaleksi@users.noreply.github.com>
2023-11-19 17:43:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
+
+//variables
+
+const times = document.querySelectorAll('.time');
+const notifications = document.querySelector('.notifications');
+const unreadMessages = document.querySelectorAll('.red');
+const markAsRead = document.querySelector('.mark');
+const redDot1 = document.querySelector('.red-dot1');
+const redDot2 = document.querySelector('.red-dot2');
+const redDot3 = document.querySelector('.red-dot3');
+
+let redDots = [redDot1, redDot2, redDot3];
+
+
 // added opaque styling to all the time element
 //with foreach method
-const times = document.querySelectorAll('.time');
-
 times.forEach((time) =>{
     time.classList.add('opaque');
+    
 })
 
-
-const unreadMessages = document.querySelectorAll('.red');
-
+//added colors to unreadmessages
 unreadMessages.forEach((unread) =>{
     unread.style.backgroundColor = '#f6fafd' ;
 })
+
+
+//function to clear unread messages
+function clear(){
+    unreadMessages.forEach((unread) =>{
+        unread.style.backgroundColor = '#ffff';
+    })
+    notifications.style.display = 'none';
+    notifications.innerHTML = '';
+    
+    for (i = 0; i < redDots.length; i++){
+        redDots[i].style.display = 'none';
+    }
+    
+    
+    
+}
+
+markAsRead.addEventListener('click', clear)
+
+
+
+
+
+
+
